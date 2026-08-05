70a156eead8c7c97f7aad3e0e800dcb54d0827f3
= <=>
2026-08-05 15:38:46 +0530

    Updates

---
+++ src/com/student/management/system/oops/Runner.java
@@ -0,0 +1,12 @@
+package com.student.management.system.oops;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1 = new Student();
+		
+
+	}
+
+}
--- src/com/student/management/system/oops/Student.java
+++ src/com/student/management/system/oops/Student.java
@@ -2,4 +2,17 @@ package com.student.management.system.oops;
 
 public class Student {
 
+	String name;
+	int rollNo;
+	int age;
+	double marksInMaths;
+	double marksInSceince;
+	double marksInEnglish;
+	String grade;
+	
+	public void calculateTotalMarks() {
+		double totalMarks = marksInMaths +marksInSceince+marksInEnglish;
+		System.out.println("total marks :"+ totalMarks);
+	}
+	
 }
